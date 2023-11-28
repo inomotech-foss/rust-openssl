6314e9c2049adb8ce63134b4a2017b28c85c2905
Simon Berger <simon.berger@inomotech.com>
2023-11-28 09:29:43 +0000

    use openssl version 111

--- openssl-sys/build/find_vendored.rs
+++ openssl-sys/build/find_vendored.rs
@@ -4,12 +4,12 @@ use std::path::PathBuf;
 use super::env;
 
 pub fn get_openssl(_target: &str) -> (Vec<PathBuf>, PathBuf) {
-    let openssl_config_dir = env("OPENSSL_CONFIG_DIR");
+    // let openssl_config_dir = env("OPENSSL_CONFIG_DIR");
 
     let mut openssl_src_build = openssl_src::Build::new();
-    if let Some(value) = openssl_config_dir {
-        openssl_src_build.openssl_dir(PathBuf::from(value));
-    }
+    // if let Some(value) = openssl_config_dir {
+    //     openssl_src_build.openssl_dir(PathBuf::from(value));
+    // }
 
     let artifacts = openssl_src_build.build();
     println!("cargo:vendored=1");
